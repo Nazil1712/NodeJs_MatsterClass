3e8edb405a710256bed0f1f3b85215808bf5bd14
Nazil1712 <ndhalwala@gmail.com>
2024-08-23 16:32:45 +0530

    Ch2

--- index.js
+++ index.js
@@ -4,9 +4,20 @@ const fs = require('fs')
 
 const index = fs.readFileSync('./index.html','utf-8')
 const data = JSON.parse(fs.readFileSync('./data.json','utf8'))
-const product = data.products[0]
+const products = data.products
 
 const server = http.createServer((req,res)=>{
+    if(req.url.startsWith('/product')) { 
+        const id = req.url.split('/')[2];
+        const product = products.find((v,i,arr)=>v.id==(+id))
+        res.writeHead(200,{'Content-Type':'text/html'})
+            let newIndex = index.replace('**title**',product.title)
+            .replace('**price**',product.price)
+            .replace('**rating**',product.rating)
+            res.end(newIndex)
+            return;
+    }
+
     switch(req.url) {
         case '/':
             res.setHeader("Content-Type",'text/html');
@@ -15,7 +26,7 @@ const server = http.createServer((req,res)=>{
 
         case '/api':
             res.writeHead(200,{'Content-Type':'application/json'})
-            res.end(JSON.stringify(data))
+            res.end(JSON.stringify(product))
             break;
 
         default:
