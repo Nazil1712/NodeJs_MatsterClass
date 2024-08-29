c5a8dd7916e91c9512998ec0495c87d491b16bca
Nazil1712 <ndhalwala@gmail.com>
2024-08-29 14:58:37 +0530

    Ch6 MongoDB Basics & Atlas

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require('dotenv').config()
 const express = require("express")
 const app = express()
 const productRouter = require('./routes/product')
 const userRouter = require('./routes/user')
 
+console.log(process.env)
 
 /* Middleware */
 app.use(express.json()) // ==> Used to parse JSON data from body
@@ -11,6 +13,6 @@ app.use('/products',productRouter)
 app.use('/users',userRouter)
 
 
-app.listen(8080,()=>{
-    console.log("Listening on port 8080")
+app.listen(process.env.PORT,()=>{
+    console.log(`Listening on port ${process.env.PORT}`)
 })
